91ac2a8b8db98b51c167ab082c6a3d80bbcaab6a
jmeyer-vlsystems <92323513+jmeyer-vlsystems@users.noreply.github.com>
2024-01-15 19:19:52 +0200

    WIP

--- index.php
+++ index.php
@@ -9,37 +9,35 @@ $bootstrap = realpath(__DIR__ . "/vendor/ion/packaging/bootstrap.php");
 if(!empty($bootstrap))
     require_once($bootstrap);
 
-if(class_exists("\\Ion\\Package")) {
+\Ion\Package::create("ion", "autoloader", function($package) {
 
-    \Ion\Package::create("ion", "autoloader", function($package) {
-
-        spl_autoload_register(function( /* string */ $className) {
-        
-            $dirs = [
-                
-                'source/classes',
-                'source/interfaces',
-                'source/traits',
-                'builds/' . PHP_MAJOR_VERSION . '.' . PHP_MINOR_VERSION,
-                'builds/' . PHP_MAJOR_VERSION,
-            ];
-        
-            foreach($dirs as $dir) {
+    spl_autoload_register(function( /* string */ $className) {
+    
+        $dirs = [
             
-                $classPath = __DIR__ . DIRECTORY_SEPARATOR . $dir . DIRECTORY_SEPARATOR . str_replace("\\", DIRECTORY_SEPARATOR, $className) . '.php';
+            'source/classes',
+            'source/interfaces',
+            'source/traits',
+            'builds/' . PHP_MAJOR_VERSION . '.' . PHP_MINOR_VERSION,
+            'builds/' . PHP_MAJOR_VERSION,
+        ];
+    
+        foreach($dirs as $dir) {
+        
+            $classPath = __DIR__ . DIRECTORY_SEPARATOR . $dir . DIRECTORY_SEPARATOR . str_replace("\\", DIRECTORY_SEPARATOR, $className) . '.php';
+            
+            $classPath = realPath($classPath);
+            
+            if (file_exists($classPath)) {
                 
-                $classPath = realPath($classPath);
-                
-                if (file_exists($classPath)) {
-                    
-                    require_once($classPath);
-                    break;
-                }
+                require_once($classPath);
+                break;
             }
-            
-        }, true, true);
+        }
         
-        return null;
+    }, true, true);
+    
+    return null;
 
-    }, __FILE__);
-}
+}, __FILE__);
+
